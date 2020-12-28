ece8a25c08aef8aba359a4e24ed295861c5c4fdd
kpitzen <kyle.pitzen@gmail.com>
2020-12-27 20:32:32 -0500

    Adds formatting rules to CI and formats repo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use actix_web::{ App, HttpServer, web, middleware::Logger };
-use sqlx::postgres::PgPoolOptions;
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use dirs;
+use sqlx::postgres::PgPoolOptions;
 
 mod config;
 mod handlers;
@@ -16,41 +16,34 @@ async fn main() -> Result<(), std::io::Error> {
 
     let db_connection_string = format!(
         "postgres://{username}:{password}@{host}:{port}/{database}",
-        username=app_config.postgres.username,
-        password=app_config.postgres.password,
-        host=app_config.postgres.host,
-        port=app_config.postgres.port,
-        database=app_config.postgres.database
+        username = app_config.postgres.username,
+        password = app_config.postgres.password,
+        host = app_config.postgres.host,
+        port = app_config.postgres.port,
+        database = app_config.postgres.database
     );
     let pool = PgPoolOptions::new()
-        .connect(&db_connection_string).await.expect(
-            "Failed to connect to DB"
-        );
+        .connect(&db_connection_string)
+        .await
+        .expect("Failed to connect to DB");
     sqlx::migrate!("./migrations")
         .run(&pool)
-        .await.expect(
-            "Failed to run migrations"
-        );
+        .await
+        .expect("Failed to run migrations");
 
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
-    let server = HttpServer::new(||
+    let server = HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
             .app_data(web::JsonConfig::default())
-            .route("/hey",
-                web::post().to(
-                    handlers::hello::manual_hello
-                )
-            )
-    );
+            .route("/hey", web::post().to(handlers::hello::manual_hello))
+    });
     let server_address = format!(
         "{host}:{port}",
-        host=app_config.app_config.host,
-        port=app_config.app_config.port
+        host = app_config.app_config.host,
+        port = app_config.app_config.port
     );
-    server.bind(server_address)?
-        .run()
-        .await
+    server.bind(server_address)?.run().await
 }
